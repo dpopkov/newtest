085cf5564691f6f2cb31b24c4da96ba5c2d59518
dpopkov <pkvdenis@gmail.com>
2017-10-31 20:53:47 +0300

    use Math.random()

--- src/main/java/Foo2.java
+++ src/main/java/Foo2.java
@@ -1,5 +1,6 @@
 public class Foo2 {
     public String bar2() {
-        return "Git is good!";
+        String s = Math.random() > 0.3 ? "good" : "bad";
+        return "Git is " + s + "!";
     }
 }
