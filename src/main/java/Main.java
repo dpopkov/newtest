ab315211e043e473447151b6e061491ad597054c
dpopkov <pkvdenis@gmail.com>
2017-10-31 21:07:24 +0300

    add a few lines to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println(new Foo().bar());
+        System.out.println("Start of task002");
         System.out.println(new Foo2().bar2());
+        System.out.println("Finish of task002");
+
     }
 }
