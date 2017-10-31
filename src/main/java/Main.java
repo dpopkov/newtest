bb56c98a8f6eca308865c3f7ee7eee969a6f5f0e
dpopkov <pkvdenis@gmail.com>
2017-10-31 21:54:55 +0300

    one more check

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,6 @@ public class Main {
         System.out.println(new Foo2().bar2());
         System.out.println(new Foo().bar());
         System.out.println("Finish of task002");
-
+        System.out.println("One more check");
     }
 }
