0f7b5a52e1fe7872c6e306ecfd6d5e6e919cda52
dpopkov <pkvdenis@gmail.com>
2017-10-31 21:04:21 +0300

    add Foo3, modify .gitignore

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(new Foo().bar());
         System.out.println(new Foo2().bar2());
+        System.out.println(new Foo().bar());
     }
 }
